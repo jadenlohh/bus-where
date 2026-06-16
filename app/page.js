c2ea171bbc3fa489bdd1a5c97f942ebb085e46a9
jaden <jadenlohh@gmail.com>
2026-06-16 20:51:42 +0800

    chore: rename file using camelCase

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
-import Navbar from "./components/navbar";
-import BusArrival from "./components/bus-arrival";
+import Navbar from "./components/Navbar";
+import BusArrival from "./components/BusArrival";
 
 export default async function Home() {
   const data = await fetch(
