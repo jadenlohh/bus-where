df9b5231a6d2442c8be36e570f40831e3a6be954
jaden <jadenlohh@gmail.com>
2026-06-17 09:14:53 +0800

    refactor: prevent caching of data by vercel CDN

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, no-cache",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
