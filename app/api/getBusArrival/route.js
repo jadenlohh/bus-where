80a2039f3680290f835f2a0eed6e327589dc41d8
jaden <jadenlohh@gmail.com>
2026-06-18 09:38:58 +0800

    feat: update GET function to accept request parameter

--- app/api/getBusArrival/route.js
+++ app/api/getBusArrival/route.js
@@ -1,6 +1,8 @@
-export async function GET() {
+export async function GET(request) {
+  const { searchParams } = new URL(request.url);
+
   const data = await fetch(
-    `https://datamall2.mytransport.sg/ltaodataservice/v3/BusArrival?BusStopCode=53009`,
+    `https://datamall2.mytransport.sg/ltaodataservice/v3/BusArrival?BusStopCode=${searchParams.get("busStopCode")}`,
     {
       headers: {
         AccountKey: process.env.API_KEY,
